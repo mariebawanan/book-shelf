0daba11ed6f7f0bfcb402d0bca86cfcc43960813
mariebawanan <bawananmarieflor@gmail.com>
2019-03-17 18:28:28 +0800

    Added side bar components, book list and book page

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -3,14 +3,54 @@ import axios from 'axios';
 export function getBooks(
     limit = 10,
     start = 0,
-    order = 'asc'
+    order = 'asc',
+    list = ''
 ) {
-    const request = `/api/books?limit=${limit}&skip=${start}&order=${order}`;
-    //const request = axios.get(`/api/books?limit=${limit}&skip=${start}&order=${order}`);
-    console.log(request);
+    const request = axios.get(`/api/books?limit=${limit}&skip=${start}&order=${order}`)
+                    .then(response => {
+                        if(list){
+                            return [...list, ...response.data]
+                        }
+                        else {
+                            return response.data
+                        } 
+                    });
+    
 
     return {
         type: 'GET_BOOKS',
-        payload: null
+        payload: request
+    }
+}
+
+export function getBookWithReviewer(id) {
+
+    const request = axios.get(`/api/getBook?id=${id}`);
+    return (dispatch) => {
+        request.then(({data}) => {
+            let book = data;
+
+            axios.get(`/api/getReviewer?id=${book.ownerId}`)
+            .then(({data}) => {
+                let response = {
+                    book,
+                    reviewer: data
+                }
+                dispatch({
+                    type: 'GET_BOOK_W_REVIEWER',
+                    payload: response
+                })
+            })
+        })
+    }
+}
+
+export function clearBookWithReviewer(){
+    return {
+        type: 'CLEAR_BOOK_W_REVIEWER',
+        payload: {
+            books: {},
+            reviewer: {}
+        }
     }
 }
--- client/src/containers/home_container.js
+++ client/src/containers/home_container.js
@@ -2,16 +2,36 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { getBooks } from '../actions';
 
+import BookItem from '../widgetsUI/book_item';
+
 class HomeContainer extends Component {
 
     componentWillMount(){
-        this.props.dispatch(getBooks(3, 0, 'desc'));
+        this.props.dispatch(getBooks(1, 0, 'desc'));
+    }
+
+    renderItems = (books) => (
+        books.list ? 
+            books.list.map( item => (
+                <BookItem {...item} key={item._id} />
+            ))
+        : null
+    )
+
+    loadMore = () => {
+        let count = this.props.books.list.length;
+        this.props.dispatch(getBooks(1, count, 'desc', this.props.books.list));
     }
 
     render() {
+        
+        console.log(this.props)
         return (
             <div>
-                home items
+                {this.renderItems(this.props.books)}
+                <div 
+                    className="loadmore"
+                    onClick={this.loadMore}>Load More</div>
             </div>
         );
     }
